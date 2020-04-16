35c5ffc8d2de5ead21fc29a24b86309690b892a9
monan <vhypnus@gmail.com>
2020-04-16 14:22:22 +0800

    update

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -28,3 +28,14 @@ func TestHeapSort(t *testing.T) {
 	HeapSort(data)
 	fmt.Println(data)
 }
+
+func TestQuicksort(t *testing.T) {
+	var data []int = []int{6,3,50,7,1,14,7,9,81}
+	quicksort(data,0,8)
+	fmt.Println(data)
+
+
+	data = []int{1,2,3,4,5,6}
+	quicksort(data,0,5)
+	fmt.Println(data)
+}
