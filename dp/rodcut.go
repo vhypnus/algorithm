95afe79496ad73888e175698f6913d4eff5afb47
monan <vhypnus@gmail.com>
2020-04-17 11:17:30 +0800

    dp

--- dp/rodcut.go
+++ dp/rodcut.go
@@ -4,6 +4,8 @@ package main
 
 import "fmt"
 
+var caches = []int{0,0,2,5,5,7,10,10,12,15,15}
+
 func max(a, b int) int {
 	if a > b {
 		return a
@@ -13,41 +15,102 @@ func max(a, b int) int {
 
 }
 
-// solve the problem recursively: initial approach
-func cutRodRec(price []int, length int) int {
-	if length == 0 {
+
+func packM(values []int,capacitys []int,capacity int) int{
+	if values == nil || len(values) == 0 {
 		return 0
 	}
 
-	q := -1
-	for i := 1; i <= length; i++ {
-		q = max(q, price[i]+cutRodRec(price, length-i))
+	var max int 
+
+	for i,v := range values {
+		var sum int
+		if capacitys[i] <= capacity {
+				
+			if capacity-capacitys[i] < len(caches)  {
+				sum = v + caches[capacity-capacitys[i]]
+			} else {
+				sum = v + pack(values,capacitys,capacity - capacitys[i])
+			}
+		} else {
+			continue
+		}
+
+		if sum > max {
+			max = sum
+		}
+		
 	}
-	return q
+
+	return max ;
 }
 
-// solve the same problem using dynamic programming
-func cutRodDp(price []int, length int) int {
-	r := make([]int, length+1) // a.k.a the memoization array
-	r[0] = 0                   // cost of 0 length rod is 0
-
-	for j := 1; j <= length; j++ { // for each length (subproblem)
-		q := -1
-		for i := 1; i <= j; i++ {
-			q = max(q, price[i]+r[j-i]) // avoiding recursive call
-		}
-		r[j] = q
+func pack(values []int,capacitys []int,capacity int) int{
+	if values == nil || len(values) == 0 {
+		return 0
 	}
 
-	return r[length]
+	var max int 
+
+	for i,v := range values {
+		var sum int
+		if capacitys[i] <= capacity {
+
+			sum = v + pack(values,capacitys,capacity - capacitys[i])
+
+		} else {
+			continue
+		}
+
+		if sum > max {
+			max = sum
+		}
+		
+	}
+
+	return max ;
+}
+
+
+func topDown(values []int,capacitys []int,capacity int) int{
+
+	var max int
+	for i:= capacity ; i > 0 ; i++ {
+		if caches[i] > 0 {
+			return caches[i]
+		}else {
+			for c,v := range caches {
+				if c > capacity {
+					break
+				}
+				var sum int 
+
+				sum = v + pack(values,capacitys,capacity - c) 
+
+				if sum > max {
+					max = sum
+				}
+			}
+		}
+		 
+	}
+
+	return max
 }
 
 
 func main() {
-	length := 10
-	price := []int{0, 1, 5, 8, 9, 17, 17, 17, 20, 24, 30}
-	// price := []int{0, 10, 5, 8, 9, 17, 17, 17, 20, 24, 30}
-	// fmt.Print(price[5]+price[length-5], "\n")
-	fmt.Print(cutRodRec(price, length), "\n")
-	fmt.Print(cutRodDp(price, length), "\n")
+	values := []int{2, 5, 3} //物品大小
+	capacitys := []int{2, 3, 4} //物品价值
+	// goods := [][]int{
+	// 	[]int{7, 2},
+	// 	[]int{5, 3},
+	// 	[]int{8, 4},
+	// }
+
+	
+
+	fmt.Println(packM(values, capacitys, 60))
+	fmt.Println(pack(values, capacitys, 60))
+	
 }
